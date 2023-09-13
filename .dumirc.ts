1a0c200345b6eb011303b6a8afd615cc0856bc26
GeorgeH <default_hyn@163.com>
2023-09-13 18:12:17 +0800

    chore: update config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,61 @@
 import { defineConfig } from 'dumi';
+//https://d.umijs.org/config配置文档
+import { SiteThemeConfig } from 'dumi-theme-antd-style';
+import { featuresZh } from './config/features';
+import { name } from './package.json';
+const themeConfig: SiteThemeConfig = {
+  name,
+  logo: 'https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png',
 
+  hero: {
+    'zh-CN': {
+      description: 'React组件库',
+      actions: [
+        {
+          type: 'primary',
+          text: '开始使用',
+          link: '/guide',
+        },
+        {
+          text: 'Github',
+          link: 'https://github.com/GeorgeHcc/g-design',
+          openExternal: true,
+        },
+      ],
+      features: featuresZh,
+    },
+    'en-US': {
+      description: 'dumi2 theme similar to antd v5 website',
+      actions: [
+        {
+          type: 'primary',
+          text: 'Start',
+          link: '/guide-en',
+        },
+        {
+          text: 'Config',
+          link: '/config-en',
+        },
+      ],
+    },
+  },
+  // socialLinks: { github: homepage },
+  apiHeader: {
+    pkg: name,
+    sourceUrl: `{github}/tree/master/src/components/{atomId}/index.tsx`,
+    docUrl: `{github}/tree/master/example/docs/components/{atomId}.{locale}.md`,
+  },
+  footer: ` Made with ❤️ by every ${name} members `,
+};
 export default defineConfig({
   outputPath: 'docs-dist',
-  themeConfig: {
-    name: 'g-design',
+  themeConfig,
+  resolve: {
+    atomDirs: [
+      {
+        type: 'component',
+        dir: 'src/components',
+      },
+    ],
   },
 });
---
+++ config/features.ts
@@ -0,0 +1,59 @@
+import { IFeature } from "dumi-theme-antd-style";
+
+export const featuresZh: IFeature[] = [
+  {
+    title: "更加轻量级",
+    image: "💠",
+    description:
+      "无需引入整个库，支持TreeShaking、按需引入",
+    row: 6,
+  },
+  {
+    title: "智能互联",
+    description:
+      "不仅仅是一个组件，所有组件都能自动互联",
+    link: "/guide/style",
+    imageType: "light",
+    image:
+      "https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png",
+    row: 9,
+  },
+  // {
+  //   title: "@doctors/npm-pkg",
+  //   link: "/guide/theme",
+  //   description:
+  //     "专注于 npm 包以及类库研发场景下的质量检测保障，为你的 npm 包开发、构建、发布流程保驾护航，尤其适用于使用 babel、tsc、swc、father 等类库平行编译场景",
+  //   image:
+  //     "https://mdn.alipayobjects.com/huamei_rqvucu/afts/img/A*8KE7T7l39J0AAAAAAAAAAAAADoN6AQ/original",
+  //   imageType: "primary",
+  // },
+  // {
+  //   title: "3分钟开发一个独一无二的预设 by create-doctor",
+  //   description:
+  //     "本脚手架专注于 DX，旨在为开发者提供只专注于 doctors features 开发的研发环境，得益于 @doctors/core 提供的无限拓展能力以及可插拔系统，请尽情发挥你的创造力!<quotient>专注于优质的 Doctors 预设/拓展包 开发体验</quotient>",
+  //   link: "/guide/syntax-highlighter",
+  //   image:
+  //     "https://mdn.alipayobjects.com/huamei_rqvucu/afts/img/A*1qLNRrRGFsQAAAAAAAAAAAAADoN6AQ/original",
+  //   imageType: "primary",
+  //   row: 9,
+  // },
+  // {
+  //   title: "所有项目都推荐添加的 @doctors/common",
+  //   description:
+  //     "本预设包含所有 Web 开发工程化项目都具备的研发质量保障流程，预计将融入 @doctors/eslint、@doctors/pkg、幽灵依赖检测等预设",
+  //   image:
+  //     "https://mdn.alipayobjects.com/huamei_rqvucu/afts/img/A*6sjjRa7lLhAAAAAAAAAAAAAADoN6AQ/original",
+  //   imageType: "primary",
+  //   link: "/components",
+  //   row: 8,
+  // },
+  // {
+  //   title: "ToDoList - Welcome to contribution 💪",
+  //   description:
+  //     "Task 列表位于 Github 的 第一个 issue 中<quotient>To be continuing 💻</quotient>",
+  //   image: "💗",
+  //   imageType: "light",
+  //   row: 6,
+  //   hero: true,
+  // },
+];
